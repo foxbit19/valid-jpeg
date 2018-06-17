1e5b9863d1af985610ab40864fcd64d16cf92bdd
foxbit19 <mattia.peretti@gmail.com>
2018-06-18 00:15:42 +0200

    Insert mocha into npm test script. Refactor tests

--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
 var assert = require('assert');
-var imageValid = require('../../');
+var imageValid = require('../');
 
 describe('valid-jpeg', function () {
     it('should set 2nd callback args to true when the image is a valid jpeg image', function () {
-        imageValid.isValid('valid.jpg', (err, valid) => {
-            assert(valid, true);
+        imageValid.isValid('test/valid.jpg', (err, valid) => {
+            assert.equal(valid, true);
         });
     });
 
-    it('should set 2nd callback args to false when the image is a valid jpeg image', function () {
-        imageValid.isValid('not_valid.jpg', (err, valid) => {
-            assert(valid, true);
+    it('should set 2nd callback args to false when the image is NOT a valid jpeg image', function () {
+        imageValid.isValid('test/not_valid.jpg', (err, valid) => {
+            assert.equal(valid, false);
         });
     });
 });
